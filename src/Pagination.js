2ce87b5af3a633149ff5c03356f1447e98d2a9ae
NC <sarasaii@amazon.com>
2024-06-13 00:08:52 +0530

    alert removed for success

--- src/Pagination.js
+++ src/Pagination.js
@@ -25,7 +25,7 @@ export default function Pagination() {
                 let data = await response.json()
                 console.log(data)
                 setData(data)
-                alert('fetched data')
+                // alert('fetched data')
             } catch(error) {
                 console.log(error)
                 alert('Failed to fetch data')
